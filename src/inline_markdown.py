bef983a9eeccfdd60a6cfb2252675f3e3eaead1f
romas <romas@gmail.com>
2025-05-10 04:57:51 +0300

    add node split for images and links

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -14,7 +14,7 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
 
         if start_index == -1:
             new_nodes.append(old_node)
-            continue 
+            continue
 
         end_index = text.find(delimiter, start_index + len(delimiter))
 
@@ -22,8 +22,8 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             raise ValueError(f"No closing delimiter found for {delimiter}")
         if start_index > 0:
             new_nodes.append(TextNode(text[:start_index], TextType.TEXT))
-        
-        new_nodes.append(TextNode(text[start_index + len(delimiter): end_index], text_type))     
+
+        new_nodes.append(TextNode(text[start_index + len(delimiter): end_index], text_type))
         end_text = text[end_index + len(delimiter):]
 
         if end_text:
@@ -39,3 +39,54 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):
     return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)",text)
 
+def split_nodes_image(old_nodes):
+    new_nodes = []
+
+    if not old_nodes:
+        return new_nodes
+
+    for old_node in old_nodes:
+        images = extract_markdown_images(old_node.text)
+
+        if not images:
+            new_nodes.append(old_node)
+            continue
+
+        alt, link = images[0]
+        image_text = f"![{alt}]({link})"
+        sections = old_node.text.split(image_text, 1)
+
+        if len(sections[0]) > 0:
+            new_nodes.append(TextNode(sections[0], TextType.TEXT))
+        new_nodes.append(TextNode(alt, TextType.IMAGE, link))
+
+        if len(sections) > 1 and sections[1]:
+            new_nodes.extend(split_nodes_image([TextNode(sections[1], TextType.TEXT)]))
+
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+
+    if not old_nodes:
+        return new_nodes
+
+    for old_node in old_nodes:
+        links = extract_markdown_links(old_node.text)
+
+        if not links:
+            new_nodes.append(old_node)
+            continue
+
+        text, link = links[0]
+        link_text = f"[{text}]({link})"
+        sections = old_node.text.split(link_text, 1)
+
+        if len(sections[0]) > 0:
+            new_nodes.append(TextNode(sections[0], TextType.TEXT))
+        new_nodes.append(TextNode(text, TextType.LINK, link))
+
+        if len(sections) > 1:
+            new_nodes.extend(split_nodes_link([TextNode(sections[1], TextType.TEXT)]))
+
+    return new_nodes
